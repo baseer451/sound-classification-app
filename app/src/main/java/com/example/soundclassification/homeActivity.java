c72eb0a76f7074970e3bafb3e1d6b93de5c5f55d
manhoosbilli1 <Khanthebest!23>
2020-03-26 00:38:17 +0500

    Added data fetching from firebase capabillity.

--- app/src/main/java/com/example/soundclassification/homeActivity.java
+++ app/src/main/java/com/example/soundclassification/homeActivity.java
@@ -1,33 +1,64 @@
 package com.example.soundclassification;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class homeActivity extends AppCompatActivity {
     Button signOut;
     TextView tv;
     FirebaseAuth mAuth;
     TextView user_name;
+    TextView SN,sound_name;
+
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    DatabaseReference reff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
         signOut = (Button) findViewById(R.id.signOut);
         tv = (TextView) findViewById(R.id.tv);
+        SN = (TextView) findViewById(R.id.SN);
+        sound_name = (TextView) findViewById(R.id.sound_name);
         mAuth = FirebaseAuth.getInstance();
         user_name = (TextView) findViewById(R.id.user_name);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        user_name.setText(user.getDisplayName());
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        reff = FirebaseDatabase.getInstance().getReference().child("user");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.child("sound").getValue().toString();
+                sound_name.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
